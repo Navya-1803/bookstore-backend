c9a73982948e7ffe55b20f5a19a27019702dc888
Navya <navya3989@gmail.com>
2026-08-31 14:51:56 +0530

    [Navya Raj] : Add user domain model and repository

---
+++ src/main/java/com/bookstore/user/dto/UserResponse.java
@@ -0,0 +1,37 @@
+package com.bookstore.user.dto;
+
+import com.bookstore.user.entity.Role;
+
+public class UserResponse {
+
+    private Long id;
+    private String name;
+    private String email;
+    private Role role;
+
+    public UserResponse() {
+    }
+
+    public UserResponse(Long id, String name, String email, Role role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+}
